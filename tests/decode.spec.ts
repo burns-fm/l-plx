3c15d4d731d8f429f3632bd5ab94381dfeb23986
robert-fairley <robbiefairley@gmail.com>
2022-12-22 19:54:20 -0500

    add other test stubs

---
+++ tests/decode.spec.ts
@@ -0,0 +1,16 @@
+/**
+ * © 2022-2022 Burns Recording Company
+ * Created: 22/12/2022
+ */
+
+describe('decode', () => {
+  test.skip('TODO - add tests here', () => {});
+});
+
+describe('decodeFromString', () => {
+  test.skip('TODO - add tests here', () => {});
+});
+
+describe('decodeToString', () => {
+  test.skip('TODO - add tests here', () => {});
+});
---
+++ tests/encode.spec.ts
@@ -0,0 +1,12 @@
+/**
+ * © 2022-2022 Burns Recording Company
+ * Created: 22/12/2022
+ */
+
+describe('encode', () => {
+  test.skip('TODO - add tests here', () => {});
+});
+
+describe('encodeFromString', () => {
+  test.skip('TODO - add tests here', () => {});
+});
